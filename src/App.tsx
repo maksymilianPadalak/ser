ee5be8c83a2a1c500bd072e4592da206c7b38d1a
padalak <maksymilian.padalak@nordsec.com>
2023-10-01 12:01:47 +0200

    create clock component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { FC } from "react";
 import { HeadingWithText } from "./molecules/HeadingWithText.tsx";
+import { Clock } from "./atoms/Clock.tsx";
 
 const App: FC = () => (
   <div className={"h-screen flex flex-col items-center bg-yellow-400"}>
+    <Clock />
     <HeadingWithText heading={"Ser"} text={"hmmm?"} />
   </div>
 );
