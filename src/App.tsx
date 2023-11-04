52ef3dde0ce6b9e63a93f82bcf2386b4adc0569f
padalak <maksymilian.padalak@nordsec.com>
2023-11-04 13:03:35 +0100

    update typescript, remove broken types, update clock time

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { CountdownClock } from "@/ui/organisms/CountdownClock";
 const App: FC = () => (
   <div className={"h-screen flex flex-col items-center bg-yellow-400"}>
     <HeadingWithText heading={"Ser"} text={"hmmm?"} />
-    <CountdownClock className={"mt-20"} targetDate={new Date("2023-10-30")} />
+    <CountdownClock className={"mt-20"} targetDate={new Date("2023-12-30")} />
   </div>
 );
 
