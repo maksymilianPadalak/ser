082bc9fd803b331fab227bd3924267f5c410bfc4
padalak <maksymilian.padalak@nordsec.com>
2023-11-25 22:51:54 +0100

    Adjust HeroBackgroundVideo component for mobile

--- src/ui/organisms/HeroBackgroundVideo/HeroBackgroundVideo.tsx
+++ src/ui/organisms/HeroBackgroundVideo/HeroBackgroundVideo.tsx
@@ -19,7 +19,10 @@ export const HeroBackgroundVideo: FC<HeroBackgroundVideoProps> = ({
         Your browser does not support the video tag.
       </video>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center z-10">
-        <Text className={cx("text-6xl text-white", "pulse")} text={title} />
+        <Text
+          className={cx("text-4xl xl:text-6xl text-white", "pulse")}
+          text={title}
+        />
       </div>
     </div>
   );
